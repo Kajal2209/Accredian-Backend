91ae4dab526f217b18e598fb967be602095997a4
Kajal2209 <paliwalkajal2209@gmail.com>
2024-07-09 17:47:59 +0530

    add fix

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const initializeDbConnection = async () => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       port: process.env.DB_PORT,
     });
     console.log('Connected to database');
